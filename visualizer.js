0003c31bf277c558f3d4221b25cbb7cef7b1ea09
erictran0x <40257345+erictran0x@users.noreply.github.com>
2021-02-20 22:01:23 -0800

    Halve the average speed of each car (again)

--- visualizer.js
+++ visualizer.js
@@ -7,13 +7,14 @@ function init() {
     const N = 100;
     let e = document.getElementById("electric").value;
     let t = Math.floor(100 * (1 - result / E));
+    console.log(t);
     let test = 0;
     for (let i = 0; i < N; ++i) {
         let obj = document.createElement("object");
         obj.data = i < e ? "greencar.svg" : "redcar.svg";
         obj.width = 100;
         obj.style = `
-            animation: move_${Math.floor(rnd(5))} ${2+rnd(6)}s linear ${rnd(3)}s infinite;
+            animation: move_${Math.floor(rnd(5))} ${4+rnd(12)}s linear ${rnd(3)}s infinite;
             opacity: ${(i % (N/t) < 1) ? 0.25 : 1};
         `;
         vis.appendChild(obj);
